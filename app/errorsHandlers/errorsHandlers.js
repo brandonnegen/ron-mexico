df78ddab6304bd19bedfb596af3c6897370d65c1
EmixMaxime <emixmaxime@outlook.fr>
2017-03-13 21:47:02 +0100

    Add mongo/joi errors handler, fix developmentErrors for api usage

--- app/errorsHandlers/errorsHandlers.js
+++ app/errorsHandlers/errorsHandlers.js
@@ -15,6 +15,67 @@ export const catchErrors = function (fn) {
 
 /**
  * 
+ * MongoDB Validation Error Handler
+ * Detect if there are mongodb validation errors that we can nicely return
+ * 
+ */
+
+export const mongoValidationErrors = (err, req, res, next) => {
+
+  /**
+   * Duplicate key error
+   * Issue : https://github.com/Automattic/mongoose/issues/2129
+   */
+  if (err.code === 11000) {
+    const regex = /index\:\ (?:.*\.)?\$?(?:([_a-z0-9]*)(?:_\d*)|([_a-z0-9]*))\s*dup key/i;    
+    const match =  err.message.match(regex);
+    const indexName = match[1] || match[2];
+
+    const error = {};
+    error[indexName] = `ce champ doit être unique. Il est déjà utilisé dans notre base.`;
+    return next({ message: error, status: 400 });
+  }
+  
+  if (!err.errors) return next(err); // It's not a mongo error
+
+  const errors = {};
+  const fields = Object.keys(err.errors);
+
+  fields.forEach(field => {
+    error[field] = error[field] || {};
+    error[field] = err.errors[field].message;
+  });
+
+  return next({ message: errors, status: 400 });
+};
+
+/**
+ * 
+ * Joi Validation Error Handler
+ * Detect if there are Joi validation errors that we can nicely return
+ * 
+ */
+export const joiValidationErrors = (err, req, res, next) => {
+  const details = err.details;
+  if (!details) return next(err);
+
+  const errors = {};
+
+  /**
+   * Joi error format :
+   * detail.message, detail.path
+   * Documentation : https://github.com/hapijs/joi/blob/v10.2.2/API.md#errors
+   */
+  details.forEach(detail => {
+    errors[detail.path] = detail.message;
+  });
+
+  return next({ message: errors, status: 400 });
+};
+
+
+/**
+ * 
  * Development Error Hanlder
  * In development we show good error messages so if we hit a syntax error or any other previously un-handled error,
  * we can show good info on what happened
@@ -29,16 +90,22 @@ export const developmentErrors = function (err, req, res, next) {
     stackHighlighted: err.stack.replace(/[a-z_-\d]+.js:\d+:\d+/gi, '<mark>$&</mark>')
   };
   
-  res.status(err.status || 500);
+  const levelName = levelNumberToString(err.level || 1);
+  logger.log(levelName, errorDetails); // Print in the terminal
+  
+  return res.status(err.status || 500).json(errorDetails.message);
+
+  /** Use the following lines if you're using express with template rendering */
   /** Based on the `Accept` http header : http://expressjs.com/en/api.html#res.format  */
+  /*
   res.format({
     // If the header is not specified, the first callback is invoked = app/json
     'application/json': () => res.json(errorDetails), // Ajax call, send JSON back
     'text/html': () => {
-      // res.render('error', errorDetails);
+      res.render('error', errorDetails);
     },
     'default': () => res.status(406).send('Not Acceptable'),
-  });
+  }); */
 };
 
   
@@ -46,18 +113,16 @@ export const developmentErrors = function (err, req, res, next) {
  * 
  * Production Error Hanlder
  * No stacktraces are leaked to user but all errors going to logger
+ * 
  */
 export const productionErrors = function (err, req, res, next) {
 
-  const levelName = levelNumberToString(err.level || 1); // Just in case we forgot to pass by custom EmixError which defines the level
+  const levelName = levelNumberToString(err.level || 1);
   
   const data = levelName === 'verbose' || levelName === 'debug'
     ? { stack: err.stack, payload: err.payload }
     : err.payload;
 
-  console.log('level name :', levelName);
-
   logger.log(levelName, data);
-
-  res.sendStatus(err.status || 500);
+  return res.sendStatus(err.status || 500);
 };
