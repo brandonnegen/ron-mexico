f3262a32f240bbde7ea3fadcf3f646da4b080781
EmixMaxime <emixmaxime@outlook.fr>
2017-03-14 17:57:24 +0100

    Use the joi/mongo errors handler

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import routes from './app/http/routes/routes';
-import { developmentErrors, productionErrors } from './app/errorsHandlers/errorsHandlers';
+import { developmentErrors, productionErrors, joiValidationErrors, mongoValidationErrors } from './app/errorsHandlers/errorsHandlers';
 import getConfig from './app/getConfig';
 import chalk from 'chalk';
 
@@ -22,6 +22,16 @@ app.use(cookieParser());
 
 /** Mount routes */
 app.use('/birds', routes);
+
+/**
+ * After adding the routes, adding the errorHandler middleware
+ */
+
+/**
+ * One of our error handlers will see if these errors are just validation errors
+ */
+app.use(joiValidationErrors);
+app.use(mongoValidationErrors);
 
 /** Error handler */
 if (appConfig.env === 'dev') {
